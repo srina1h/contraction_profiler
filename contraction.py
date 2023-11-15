20d8748d998eacc6d3b19028dd93984e5d8c8d9b
Srinath Srinivasan <ssrini27@ncsu.edu>
2023-11-15 11:02:08 -0500

    making arg callable

--- contraction.py
+++ contraction.py
@@ -41,7 +41,7 @@ def con():
         cutensor.contraction(alpha, a, desc_a, mode_a, b, desc_b, mode_b, beta, c, desc_c, mode_c)
 
 torch.cuda.cudart().cudaProfilerStart()
-perf = cupyx.time.repeat(con(),n_warmup=1, n_repeat=5)
+perf = cupyx.time.repeat(con,n_warmup=1, n_repeat=5)
 torch.cuda.cudart().cudaProfilerStop()
 
 total_flops = 2 * numpy.prod(numpy.array(list(extent.values())))
