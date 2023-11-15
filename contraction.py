330882295e1627de99186796cc5b9dc78add621f
Srinath Srinivasan <ssrini27@ncsu.edu>
2023-11-15 11:31:15 -0500

    gett

--- contraction.py
+++ contraction.py
@@ -7,17 +7,17 @@ import torch
 
 dtype = numpy.float32
 
-mode_a = ('a', 'b')
-mode_b = ('b', 'c', 'd')
-mode_c = ('a', 'c', 'd')
-extent = {'a': 12, 'b': 20, 'c': 8, 'd': 20}
-con_type = "ab * bcd -> acd"
+# mode_a = ('a', 'b')
+# mode_b = ('b', 'c', 'd')
+# mode_c = ('a', 'c', 'd')
+# extent = {'a': 12, 'b': 20, 'c': 8, 'd': 20}
+# con_type = "ab * bcd -> acd"
 
-# mode_a = ('a', 'b', 'c')
-# mode_b = ('c', 'd', 'e')
-# mode_c = ('a', 'b', 'd', 'e')
-# extent = {'a': 12, 'b': 8, 'c': 20, 'd': 8, 'e': 20}
-# con_type = "abc * cde -> abde"
+mode_a = ('a', 'b', 'c')
+mode_b = ('c', 'd', 'e')
+mode_c = ('a', 'b', 'd', 'e')
+extent = {'a': 12, 'b': 8, 'c': 20, 'd': 8, 'e': 20}
+con_type = "abc * cde -> abde"
 
 a = cupy.random.random([extent[i] for i in mode_a])
 b = cupy.random.random([extent[i] for i in mode_b])
@@ -34,11 +34,11 @@ mode_a = cutensor.create_mode(*mode_a)
 mode_b = cutensor.create_mode(*mode_b)
 mode_c = cutensor.create_mode(*mode_c)
 alpha = 1.1
-beta = 1.0
+beta = 0
 
 def con():
     with nvtx.annotate(con_type, color = "purple"):
-        cutensor.contraction(alpha, a, desc_a, mode_a, b, desc_b, mode_b, beta, c, desc_c, mode_c)
+        cutensor.contraction(alpha, a, desc_a, mode_a, b, desc_b, mode_b, beta, c, desc_c, mode_c, algo = -1)
 
 torch.cuda.cudart().cudaProfilerStart()
 perf = cupyx.time.repeat(con,n_warmup=1, n_repeat=5)
