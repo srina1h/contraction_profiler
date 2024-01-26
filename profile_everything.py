ccd035f19d54bd871ecfd41af461b679e7f9c43f
Srinath Srinivasan <ssrini27@ncsu.edu>
2024-01-26 12:53:52 -0500

    trying different arrangement

--- profile_everything.py
+++ profile_everything.py
@@ -114,7 +114,8 @@ print('GFLOPS: {}'.format(total_flops / elapsed / 1e9))
 
 def con4():
     with nvtx.annotate(con_type, color = "purple"):
-        torch.tensordot(atorch,btorch.reshape(20, 768),[[1],[1]])
+        btorch = btorch.reshape(20, 768)
+        torch.tensordot(atorch,btorch, [[1],[1]])
 
 torch.cuda.cudart().cudaProfilerStart()
 perf4 = cupyx.time.repeat(con4,n_warmup=1, n_repeat=5)
