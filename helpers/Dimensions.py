c560a1dfaccdd69f1060cbfaf3e2da4e9e5911f8
Srinath Srinivasan <ssrini27@ncsu.edu>
2024-02-23 12:09:53 -0500

    none type check

--- helpers/Dimensions.py
+++ helpers/Dimensions.py
@@ -1,8 +1,9 @@
 class Dimensions:
     def __init__(self, adim: tuple, bdim: tuple, cdim: tuple, tdotConDim: tuple, con_type: str, dataType: str):
-        self.adim = adim
-        self.bdim = bdim
-        self.cdim = cdim
-        self.tdotConDim = tdotConDim
-        self.con_type = con_type
-        self.dataType = dataType
+        if adim not None and bdim not None and cdim not None and tdotConDim not None and con_type not None and dataType not None:
+            self.adim = adim
+            self.bdim = bdim
+            self.cdim = cdim
+            self.tdotConDim = tdotConDim
+            self.con_type = con_type
+            self.dataType = dataType
