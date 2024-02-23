0224e8c97984b4d21328d850bf8ab6278be05fdf
Srinath Srinivasan <ssrini27@ncsu.edu>
2024-02-23 12:10:25 -0500

    revert

--- helpers/Dimensions.py
+++ helpers/Dimensions.py
@@ -1,9 +1,8 @@
 class Dimensions:
     def __init__(self, adim: tuple, bdim: tuple, cdim: tuple, tdotConDim: tuple, con_type: str, dataType: str):
-        if adim not None and bdim not None and cdim not None and tdotConDim not None and con_type not None and dataType not None:
-            self.adim = adim
-            self.bdim = bdim
-            self.cdim = cdim
-            self.tdotConDim = tdotConDim
-            self.con_type = con_type
-            self.dataType = dataType
+        self.adim = adim
+        self.bdim = bdim
+        self.cdim = cdim
+        self.tdotConDim = tdotConDim
+        self.con_type = con_type
+        self.dataType = dataType
