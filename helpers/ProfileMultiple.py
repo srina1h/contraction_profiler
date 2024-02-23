100f3efa16e6fc1c2b6c7612ca85096300e8c7e4
Srinath Srinivasan <ssrini27@ncsu.edu>
2024-02-23 14:26:03 -0500

    updated right var

--- helpers/ProfileMultiple.py
+++ helpers/ProfileMultiple.py
@@ -29,6 +29,5 @@ class ProfileMultiple:
         df = pd.DataFrame(columns = ['con_label','default_CPU', 'default_GPU', 'ttgt_CPU', 'ttgt_GPU', 'tgett_CPU', 'tgett_GPU', 'gett_CPU', 'gett_GPU', 'default_patient_CPU', 'default_patient_GPU', 'tensordot_CPU', 'tensordot_GPU', 'correctness', 'fastest_CPU', 'fastest_GPU'], index = [i for i in range(len(self.results))])
         for result in self.results:
             print(len(result))
-            for i in result:
-                df = df._append({'con_label': i[0], 'default_CPU': i[1][0], 'default_GPU': i[1][1], 'ttgt_CPU': i[2][0], 'ttgt_GPU': i[2][1], 'tgett_CPU': i[3][0], 'tgett_GPU': i[3][1], 'gett_CPU': i[4][0], 'gett_GPU': i[4][1], 'default_patient_CPU': i[5][0], 'default_patient_GPU': i[5][1], 'tensordot_CPU': i[6][0], 'tensordot_GPU': i[6][1], 'correctness': i[7], 'fastest_CPU': i[8], 'fastest_GPU': i[9]}, ignore_index = True)
+            df = df._append({'con_label': result[0], 'default_CPU': result[1][0], 'default_GPU': result[1][1], 'ttgt_CPU': result[2][0], 'ttgt_GPU': result[2][1], 'tgett_CPU': result[3][0], 'tgett_GPU': result[3][1], 'gett_CPU': i[4][0], 'gett_GPU': result[4][1], 'default_patient_CPU': result[5][0], 'default_patient_GPU': result[5][1], 'tensordot_CPU': result[6][0], 'tensordot_GPU': result[6][1], 'correctness': result[7], 'fastest_CPU': result[8], 'fastest_GPU': result[9]}, ignore_index = True)
         df.to_csv(self.output_filepath)
