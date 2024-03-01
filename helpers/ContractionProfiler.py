9bbdd26c51c41fa7d1ccde550ce5e87c1a0f7270
Srinath Srinivasan <ssrini27@ncsu.edu>
2024-03-01 00:22:59 -0500

    checking values

--- helpers/ContractionProfiler.py
+++ helpers/ContractionProfiler.py
@@ -117,6 +117,8 @@ class ContractionProfiler:
     def check_correctness(self, algo_number) -> bool:
         cu = cutensor.contraction(self.alpha, self.a, self.mode_a, self.b, self.mode_b, self.beta, self.c, self.mode_c, algo = algo_number)
         to = torch.tensordot(self.atorch, self.btorch, dims = self.dimensions.tdotConDim)
+        print(cupy.asnumpy(cu))
+        print(to.numpy)
 
         if numpy.array_equal(cupy.asnumpy(cu), to.numpy):
             return True
