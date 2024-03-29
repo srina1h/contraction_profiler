d0a5f69ffa4bb60f7ca04083eb642c1df07cbea5
Srinath Srinivasan <ssrini27@ncsu.edu>
2024-03-29 14:36:35 -0400

    debugging

--- multidim_correctness.py
+++ multidim_correctness.py
@@ -10,8 +10,8 @@ dtype = numpy.float16
 # B reshape torch.Size([20, 768])
 # final torch.Size([4096, 20])
 
-atorch = torch.rand((4096, 768), device = 'cuda', dtype = torch.float16)
-btorch = torch.rand((20, 8, 8, 12), device = 'cuda', dtype = torch.float16)
+atorch = torch.rand((2, 4), device = 'cuda', dtype = torch.float16)
+btorch = torch.rand((3, 2, 2, 1), device = 'cuda', dtype = torch.float16)
 
 mode_a = ('a', 'b')
 mode_b = ('c', 'd', 'e', 'f')
@@ -30,9 +30,10 @@ mode_b = cutensor.create_mode(*mode_b)
 mode_c = cutensor.create_mode(*mode_c)
 alpha = 1
 beta = 0
-
-btorch = btorch.reshape(20, 768)
+print(a)
+print(b)
 cu = cutensor.contraction(alpha, a, mode_a, b, mode_b, beta, c, mode_c, algo = -4)
+btorch = btorch.reshape(20, 768)
 to = torch.tensordot(atorch, btorch, dims = ([1],[1]))
 
 if numpy.allclose(cupy.asnumpy(cu),to.cpu().numpy(), atol=1e-1, rtol=1e-1):
