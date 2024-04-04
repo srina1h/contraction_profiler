4c78a4845a2aac832bf528371e5fcc6bf881f5aa
Srinath Srinivasan <ssrini27@ncsu.edu>
2024-04-04 16:45:27 -0400

    adding default arg to baseline against

--- profilefile.py
+++ profilefile.py
@@ -6,7 +6,7 @@ def main():
     parser.add_argument('file_path', type=str, help='Path to the file to be profiled')
     parser.add_argument('output_filepath', type=str, help='Path to the file to be profiled')
     parser.add_argument('type', type=str, help='Type of file to be profiled (csv or xlsx)')
-    parser.add_argument('baseline', type=str, help='What it should be compared to (ttgt, tdot)')
+    parser.add_argument('baseline', type=str, help='What it should be compared to (ttgt, tdot)', default='tdot')
 
     args = parser.parse_args()
 
